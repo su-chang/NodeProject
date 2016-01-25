34a36513ee02f45e05f206ad92325c74427b609f
苏畅 <su_chang@yeah.net>
2016-01-25 08:57:38 +0800

    my blog system

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -3,13 +3,17 @@ var crypto = require('crypto'),
 
 module.exports = function (app) {
 	app.get('/', function(req, res) {
-		res.render('index', {title: '主页'});
+		res.render('index', {
+			title: '主页'
+		});
 	});
-
+	//注册
 	app.get('/reg', function(req, res) {
-		res.render('reg', {title: '注册'});
+		res.render('reg', {
+			title: '注册'
+		});
 	});
-
+	//响应注册
 	app.post('/reg', function(req, res) {
 		var name = req.body.name,
 			password = req.body.password,
@@ -48,21 +52,22 @@ module.exports = function (app) {
 			});
 		});
 	});
-
+	//用户登录
 	app.get('/login', function(req, res) {
 		res.render('login', {title: '登录'});
 	});
-
+	//响应登录
 	app.post('/login', function(req, res) {
 	});
-
+	//发表文章
 	app.get('/post', function(req, res) {
 		res.render('post', {title: '发表'});
 	});
-
+	//响应发表文章
 	app.post('/post', function(req, res) {
 	});
-
+	//登出
 	app.get('/logout', function(req, res) {
 	});
 };
+
